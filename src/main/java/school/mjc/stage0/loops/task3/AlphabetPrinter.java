95b046ac3be69a2e40545659c859d3661382ac0c
raman <r4m4n911@gmail.com>
2023-03-20 23:46:06 +0300

    1

--- src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.loops.task3;
 
 public class AlphabetPrinter {
     public void printAlphabet() {
+        char c = 'a';
+        while (c < 'z') {
+            System.out.println(c);
+            c++;
+        }
+        System.out.println(c);
     }
 }
--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -2,5 +2,16 @@ package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
+        int i = 0;
+        int a = 0;
+        int b = 1;
+        int c = 0;
+        while (i < lastFibonacci) {
+            System.out.println(a);
+            c = b;
+            b = a + b;
+            a = c;
+            i++;
+        }
     }
 }
